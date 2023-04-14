eed84b6172e0469ebd156713710524eb53e67496
Benjamin Stein <bxs971@student.bham.ac.uk>
2023-04-14 12:18:27 +0100

    Updated key to dict

--- app.py
+++ app.py
@@ -6,20 +6,20 @@ import json
 app = Flask(__name__)
 
 dict_key = {
-        "type": "service_account",
-        "project_id": "terragrow-1",
-        "private_key_id": "{}".format(os.environ.get('PRIVATE_KEY_ID')),
-        "private_key": "{}".format(os.environ.get('PRIVATE_KEY')),
-        "client_email": "{}".format(os.environ.get('CLIENT_EMAIL')),
-        "client_id": "{}".format(os.environ.get('CLIENT_ID')),
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "{}".format(os.environ.get('CLIENT_X509_CERT_URL'))
-    }
+    "type": "service_account",
+    "project_id": "terragrow-1",
+    "private_key_id": "{}".format(os.environ.get('PRIVATE_KEY_ID')),
+    "private_key": "{}".format(os.environ.get('PRIVATE_KEY')),
+    "client_email": "{}".format(os.environ.get('CLIENT_EMAIL')),
+    "client_id": "{}".format(os.environ.get('CLIENT_ID')),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "{}".format(os.environ.get('CLIENT_X509_CERT_URL'))
+}
 
 # Initialize Firestore DB
-cred = credentials.Certificate(dict_key)
+cred = credentials.Certificate('key.json')
 default_app = initialize_app(cred)
 db = firestore.client()
 plant_ref = db.collection('plants')
